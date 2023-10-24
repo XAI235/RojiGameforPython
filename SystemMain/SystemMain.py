90627491b76ea0816a37418b6b27935fef99e2ff
Xai Agami <32261252+XAI235@users.noreply.github.com>
2023-10-25 00:36:40 +0900

    using pygame
    using pygame
    
    Create Window
    Set Window Size
    Set Window Title

--- SystemMain/SystemMain.py
+++ SystemMain/SystemMain.py
@@ -1,24 +1,32 @@
 import sys, random
 import pygame
+from pygame.locals import *
 import tkinter
 
 class SystemMain:
 
     frm = None
     def __init__(self):
-        self.frm = tkinter.Tk()
+        #self.frm = tkinter.Tk()
         pygame.init()
     def __del__(self):
         return 
 
-    def initialize(self, screensize,titlename):
-        self.frm.geometry(screensize)
-        self.frm.title(titlename)
+    def initialize(self, width, height,screensize, titlename):
+        self.screen = pygame.display.set_mode((width, height))  # 画面サイズ設定
+        pygame.display.set_caption(titlename)                   # ウィンドウタイトル設定
+        #self.frm.geometry(screensize)s
+        #self.frm.title(titlename)
         return True
 
     def systemmain(self):
-        self.frm.mainloop()
-        return
+        while(True):
+            self.screen.fill((0,0,0,))                          # 背景色(ここをうまくやればフェードイン、フェードアウト作れる?)
+            pygame.display.update()                             # 画面更新 必ず必要
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+        #self.frm.mainloop()
 
     def finalize(self):
-        print(3+1)
+        sys.exit
