500a52e92b28440d8f50e800e4086f66665f8a64
Xai Agami <32261252+XAI235@users.noreply.github.com>
2023-11-05 21:25:15 +0900

    Setting BG and BGM
    setting BG and BGM

--- SystemMain/SystemMain.py
+++ SystemMain/SystemMain.py
@@ -3,8 +3,9 @@ import pygame
 from pygame.locals import *
 import tkinter
 
-class SystemMain:
 
+
+class SystemMain:
     frm = None
     def __init__(self):
         #self.frm = tkinter.Tk()
@@ -20,11 +21,19 @@ class SystemMain:
         return True
 
     def systemmain(self):
+        background = pygame.image.load('.\Data\image\menu.png') # とりあえずここ、画像クラスを作った方がよいのかもしれない?
+        pygame.mixer.music.load('.\Data\sound\\title.wav') # BGM読み込み
+        pygame.mixer.music.set_volume(0.1)                  # 10%にとりあえず設定
+
         while(True):
+            if(not pygame.mixer.music.get_busy()) :
+                pygame.mixer.music.play(-1)
             self.screen.fill((0,0,0,))                          # 背景色(ここをうまくやればフェードイン、フェードアウト作れる?)
+            self.screen.blit(background,(0,0))
             pygame.display.update()                             # 画面更新 必ず必要
             for event in pygame.event.get():
-                if event.type == QUIT:
+                if event.type == QUIT:                          # 終了ボタンを押した場合終了 セーブ警告とかなし
+                    pygame.mixer.music.fadeout(2)
                     pygame.quit()
         #self.frm.mainloop()
 
--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+
+import os,sys
 from SystemMain import SystemMain
+#os.path.abspath(__file__) : pythonファイルが存在する絶対パスを取得
+#os.path.dirname(): 入力したパスの一つ上の階層のパスを取得
+
+ROOTPATH = os.path.dirname(os.path.abspath(__file__))
+CFGPATH = os.path.dirname(os.path.dirname(ROOTPATH))
+
+#sys.path.append() ：入力したパスをpythonファイルの探索パスに追加する
+
+sys.path.append(CFGPATH)
 
 def main():
         SysM = SystemMain.SystemMain()
